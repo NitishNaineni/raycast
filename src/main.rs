c726b187b2876abc38e7f6d67e70893255740bc8
NitishNaineni <nitishnaineni@gmail.com>
2023-11-19 15:11:10 -0600

    simple window pop up code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let _ = event_loop.run(move |event, elwt| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                elwt.exit();
+            },
+            Event::AboutToWait => {
+                // Application update code.
+                println!("Updating application...");
+                window.request_redraw();
+            },
+            Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } => {
+                println!("Redrawing application");
+                // Redraw the application.
+            },
+            _ => ()
+        }
+    });
 }
